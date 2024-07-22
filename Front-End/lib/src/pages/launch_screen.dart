e53de3c8b97125b9058b82290cd373d302e48436
Ahzem <123859613+Ahzem@users.noreply.github.com>
2024-07-22 19:06:49 +0530

    chore: Update .gitignore files for Back-End and Front-End folders

--- Front-End/lib/src/pages/launch_screen.dart
+++ Front-End/lib/src/pages/launch_screen.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 import '../widgets/background_widget.dart';
+import 'instruction_page_1.dart'; // Make sure to import your instruction page
 
-void main() {
-  runApp(const LaunchScreen());
+class LaunchScreen extends StatefulWidget {
+  const LaunchScreen({super.key});
+
+  @override
+  _LaunchScreenState createState() => _LaunchScreenState();
 }
 
-class LaunchScreen extends StatelessWidget {
-  const LaunchScreen({super.key});
+class _LaunchScreenState extends State<LaunchScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Delay for 3 seconds before navigating to the instruction page
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const InstructionPage1()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
---
+++ Front-End/lib/src/widgets/button.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import '../constants/constants.dart';
+
+/* Custom button widget 
+  * 
+  * This widget is used to create a custom button with a primary color background
+  * and white text. The button text is passed as a parameter to the widget.
+  * 
+  * Parameters:
+  * - onPressed: VoidCallback - The function to be called when the button is pressed
+  * - text: String - The text to be displayed on the button
+  * 
+  * Example:
+  * 
+  * CustomButton(
+  *   onPressed: () {
+  *     // Add your button action here
+  *   },
+  *   text: 'Button Text',
+  * )
+  *                   CustomButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const InstructionPage2(),
+                            ),
+                          );
+                        },
+                        text: 'Next',
+                      ),
+  */
+
+// Filled button with primary color background and white text
+class CustomButtonFilled extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String text;
+
+  const CustomButtonFilled({
+    super.key,
+    required this.onPressed,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        foregroundColor: kBackgroundColor,
+        backgroundColor: kPrimaryColor,
+        padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 12),
+        textStyle: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+      child: Text(text),
+    );
+  }
+}
+
+// Outlined button with primary color border and white text
+class CustomButtonOutlined extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String text;
+
+  const CustomButtonOutlined({
+    super.key,
+    required this.onPressed,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        foregroundColor: kPrimaryColor,
+        backgroundColor: kTransparentColor,
+        padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 12),
+        textStyle: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w400,
+        ),
+        side: const BorderSide(
+          color: kPrimaryColor,
+          width: 2,
+        ),
+      ),
+      child: Text(text),
+    );
+  }
+}
