c53ae8fa7e60feea41f07e2d0e9a654e9b780ec7
Ahzem <123859613+Ahzem@users.noreply.github.com>
2024-07-27 07:27:06 +0530

    chore: Update signup page UI and functionality

--- Front-End/lib/src/pages/sign-pages/signup_page.dart
+++ Front-End/lib/src/pages/sign-pages/signup_page.dart
@@ -1,1 +1,101 @@
-// Signup page
+import 'package:flutter/material.dart';
+import 'package:seyoni/src/pages/sign-pages/components/signin_fields.dart';
+import '../../constants/constants_color.dart';
+import '../../widgets/background_widget.dart';
+import 'components/signup_fields.dart';
+
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: kTransparentColor,
+      body: BackgroundWidget(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/logo-icon.png',
+              height: height * 0.15,
+              fit: BoxFit.cover,
+            ),
+            Image.asset(
+              'assets/images/logo-name.png',
+              height: height * 0.12,
+              fit: BoxFit.cover,
+            ),
+            Container(
+              margin: const EdgeInsets.all(20),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: kContainerColor,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: kBoarderColor,
+                  width: 1.5,
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  EmailFieldSignUp(
+                    key: const Key('email-field'),
+                    controller: TextEditingController(),
+                    errorText: 'Email is incorrect',
+                  ),
+                  const SizedBox(height: 10),
+                  PasswordField(
+                    key: const Key('password-field'),
+                    controller: TextEditingController(),
+                    errorText: 'Password is incorrect',
+                  ),
+                  const SizedBox(height: 10),
+                  SignInButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/signin');
+                    },
+                  ),
+                  const Text(
+                    'Already have an account?',
+                    style: TextStyle(
+                      color: kContainerColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SignUpButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/signup');
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  SignInWithGoogleButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/home');
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  SignInWithFacebookButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/home');
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  SignInWithAppleButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/home');
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
