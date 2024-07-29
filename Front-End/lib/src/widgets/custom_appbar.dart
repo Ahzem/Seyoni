7a6b60a1264f0d100817b2d00778f1b2c76f7ff4
Ahzem <123859613+Ahzem@users.noreply.github.com>
2024-07-29 11:09:32 +0530

    Create a custom AppBar Template for all screens

--- Front-End/lib/src/widgets/custom_appbar.dart
+++ Front-End/lib/src/widgets/custom_appbar.dart
@@ -1,39 +1,46 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 import '../constants/constants_color.dart';
-import '../widgets/background_widget.dart';
 
-// custom app bar for home pages
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  final Size preferredSize;
 
-class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
+  const CustomAppBar({super.key}) : preferredSize = const Size.fromHeight(60.0);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kTransparentColor,
-      body: BackgroundWidget(
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.notifications_none_outlined,
-                      size: 24, color: kPrimaryColor),
-                  onPressed: () {},
-                ),
-                const Image(
-                  image: AssetImage('assets/images/logo.png'),
-                  height: 30,
-                  fit: BoxFit.cover,
-                ),
-                IconButton(
-                  icon: const Icon(Icons.menu_outlined,
-                      size: 24, color: kPrimaryColor),
-                  onPressed: () {},
-                ),
-              ],
+    return SafeArea(
+      child: Container(
+        decoration: const BoxDecoration(
+          color: kTransparentColor,
+        ),
+        child: ClipRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(
+                sigmaX: 10.0, sigmaY: 10.0), // Apply blur effect
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.notifications_none_outlined,
+                        size: 24, color: kPrimaryColor),
+                    onPressed: () {},
+                  ),
+                  const Image(
+                    image: AssetImage('assets/images/logo.png'),
+                    height: 35,
+                    fit: BoxFit.cover,
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.menu_outlined,
+                        size: 24, color: kPrimaryColor),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -41,6 +48,3 @@ class CustomAppBar extends StatelessWidget {
     );
   }
 }
-
-
-// custom app bar for entry pages
