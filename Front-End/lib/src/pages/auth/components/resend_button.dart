abf002a38b41f831e02b0244e71e237fbbadf699
Ahzem <123859613+Ahzem@users.noreply.github.com>
2024-08-04 14:03:55 +0530

    chore: Update OTP page with adding real timer and create new resend button

---
+++ Front-End/lib/src/pages/auth/components/resend_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../../../constants/constants_color.dart';
+
+// Resend button Active
+class ResendButton extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const ResendButton({super.key, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: kTransparentColor,
+        side: const BorderSide(color: kPrimaryColor),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 90,
+          vertical: 10,
+        ),
+        textStyle: const TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      child: const Text(
+        'Resend',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
