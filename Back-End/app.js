85632abbea9cc4045736a484b4f6897239e4e3fe
Ahzem <123859613+Ahzem@users.noreply.github.com>
2024-08-07 17:05:12 +0530

    Refactor image paths in customNavBar component

--- Back-End/app.js
+++ Back-End/app.js
@@ -1,12 +1,19 @@
 const express = require("express");
-const { connectToDatabase } = require("./config/db");
+const { connectToDatabase } = require("./config/db.js");
+const authRouter = require("./routers/authRouter");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
+
+app.use(bodyParser.json());
 
 // Connect to the database before starting the server
 connectToDatabase().catch((error) => {
   console.error("Failed to connect to the database", error);
   process.exit(1); // Exit the process with an error code
 });
+
+app.use("/api/auth", authRouter);
 
 module.exports = app;
