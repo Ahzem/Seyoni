a354b601ffaf2b1e2115bb496fc62a8bd8968239
Ahzem <123859613+Ahzem@users.noreply.github.com>
2024-10-19 11:21:24 +0530

    Update server configuration to bind to all network interfaces

--- Back-End/index.js
+++ Back-End/index.js
@@ -5,7 +5,8 @@ const app = require("./app.js");
 require("./config/db.js"); // Ensure the database connection is established
 
 const port = process.env.PORT || 3000;
+const host = "0.0.0.0"; // Bind to all network interfaces
 
-app.listen(port, () => {
+app.listen(port, host, () => {
   console.log(`Hi Ahzem, I'm listening on port ${port}!`);
 });
--- Front-End/lib/src/config/url.dart
+++ Front-End/lib/src/config/url.dart
@@ -1,4 +1,4 @@
-const url = 'http://localhost:3000';
+const url = 'http://98.83.137.96:3000';
 const registerSeekersUrl = '$url/api/seeker/signup';
 const loginSeekersUrl = '$url/api/seeker/signin';
 const verifyOtpUrl = '$url/api/seeker/verifySignUpOtp';
