aedfc60032652af649f656c533eaaf99178d8ae5
Ahzem <123859613+Ahzem@users.noreply.github.com>
2024-11-06 12:42:13 +0530

    Update URL configuration: uncomment production URLs and comment out local URLs for deployment

--- Front-End/lib/src/config/url.dart
+++ Front-End/lib/src/config/url.dart
@@ -1,7 +1,7 @@
-// const url = 'https://seyoni.onrender.com';
+const url = 'https://seyoni.onrender.com';
 const wsUrl = 'wss://seyoni.onrender.com/ws';
 // const wsUrl = 'ws://localhost:3000/ws';
-const url = 'http://localhost:3000';
+// const url = 'http://localhost:3000';
 const registerSeekersUrl = '$url/api/seeker/signup';
 const loginSeekersUrl = '$url/api/seeker/signin';
 const verifyOtpUrl = '$url/api/seeker/verifySignUpOtp';
